b2b2a38e41a8239888d0b33e47323e955aa3d4a6
Natalia Owsiejko <natalia.owsiejko@onet.pl>
2025-03-09 14:38:32 +0100

    zad2

--- lab2/zad2/app/server.js
+++ lab2/zad2/app/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 app.get('/', (req, res) => {
-  res.json({ date: new Date() });
+  res.json({ date: new Date().toLocaleString() });
 });
 app.listen(port, () => {
   console.log('Server running on http://localhost:' + port + '/');
--- lab2/zad2/zad2.sh
+++ lab2/zad2/zad2.sh
@@ -8,7 +8,7 @@ echo "const express = require('express');
 const app = express();
 const port = 8080;
 app.get('/', (req, res) => {
-  res.json({ date: new Date() });
+  res.json({ date: new Date().toLocaleString() });
 });
 app.listen(port, () => {
   console.log('Server running on http://localhost:' + port + '/');
